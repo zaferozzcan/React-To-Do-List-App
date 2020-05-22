10c3b48899ad962c2930e8099b816813221b4b2f
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-05-21 22:05:48 -0400

    Add On Change Handler for input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState("");
+
+
+  function handleOnChange(event) {
+    setItem(event.target.value);
+    console.log(item);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleOnChange} type="text" />
+        <button type="button">
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          <li> Item </li>
         </ul>
       </div>
     </div>
